6f3f0ae38f271c28465245e909188531ba5ae3ab
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-07 21:21:53 +0200

    Feature: Added a Person class and a gender enum which are used to create a list of Persons.

--- javafunctional/src/main/java/com/umer/javafunctional/imperative/Main.java
+++ javafunctional/src/main/java/com/umer/javafunctional/imperative/Main.java
@@ -1,12 +1,37 @@
 package com.umer.javafunctional.imperative;
 
+import java.util.List;
+import static com.umer.javafunctional.imperative.Main.Gender.*;
 
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        //System.out.println( "Hello World!" );
-    	
-    	
-    }
+public class Main {
+	public static void main(String[] args) {
+		List<Person> people = List.of(
+				new Person("Umer", MALE), 
+				new Person("Maria", FEMALE),
+				new Person("Aisha", FEMALE), 
+				new Person("Alex", MALE), 
+				new Person("Alice", FEMALE)
+			);
+	}
+
+	static class Person {
+		private final String name;
+		private final Gender gender;
+
+		public Person(String name, Gender gender) {
+			this.name = name;
+			this.gender = gender;
+		}
+
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return "Person {" + "name='" + name + "\'" + ", gender=" + gender + "}";
+
+		}
+	}
+
+	enum Gender {
+		MALE, FEMALE;
+	}
 }
