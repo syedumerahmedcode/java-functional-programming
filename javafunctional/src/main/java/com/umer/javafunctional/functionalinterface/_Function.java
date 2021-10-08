19a7f18436c42e46013dd39f589b1e1f86e97884
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-08 09:03:24 +0200

    Feature: Created a simple class which increments a number.

--- javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Function.java
+++ javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Function.java
@@ -1,5 +1,14 @@
 package com.umer.javafunctional.functionalinterface;
 
 public class _Function {
+	
+	public static void main(String[] args) {
+		int result = increment(1);
+		System.out.println(result);
+	}
+	
+	static int increment(int number) {
+		return number+1;
+	}
 
 }
