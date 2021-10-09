627cff9edfa11ab9e3c8af0be25d01b384fb3551
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-09 12:19:32 +0200

    Feature: Renamed the method to highlight how imperative and declarative approach map with each other.

--- javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Function.java
+++ javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Function.java
@@ -3,21 +3,21 @@ package com.umer.javafunctional.functionalinterface;
 import java.util.function.Function;
 
 public class _Function {
-	
+
 	public static void main(String[] args) {
-		int result = incrementByOne(1);
+		int result = incrementByOneFunction(1);
 		System.out.println(result);
-		
-		int result2=incrementByOneFunction.apply(1);
+
+		int result2 = incrementByOneFunction.apply(1);
 		System.out.println(result2);
 	}
-	
+
 	// declarative approach
-	static Function<Integer, Integer> incrementByOneFunction=number -> number+1;
-	
+	static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
+
 	// imperative approach
-	static int incrementByOne(int number) {
-		return number+1;
+	static int incrementByOneFunction(int number) {
+		return number + 1;
 	}
 
 }
