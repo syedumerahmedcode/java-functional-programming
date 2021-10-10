6f515faac836bc27721400f5dd0fa0f03ee7a651
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-10 09:11:25 +0200

    Feature: Added the declarative approach for greeeting a customer using Cunsumer Functional Interface.

--- javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Consumer.java
+++ javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Consumer.java
@@ -1,11 +1,24 @@
 package com.umer.javafunctional.functionalinterface;
 
+import java.util.function.Consumer;
+
 public class _Consumer {
 	
 	public static void main(String[] args) {
-		greetCustomer(new Customer("Maria", "99999"));
+		// Normal java function
+		final Customer maria = new Customer("Maria", "99999");
+		greetCustomer(maria);
+		
+		// Consumer Functional interface
+		greetCustomerConsumer.accept(maria);
 		
 	}
+	
+	// Declarative approach
+	static Consumer<Customer> greetCustomerConsumer=customer ->System.out.println(
+				"Hello " + customer.customerName 
+				+ ", thanks for registering phone number "
+				+ customer.customerPhoneNumber);
 	
 	// Imperative approach 
 	static void greetCustomer(Customer customer) {
