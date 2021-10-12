15fd40162eb7dcd2f4b8e7024cb0757247007bf8
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-12 20:02:58 +0200

    Feature: Using Supplier to return a list of connection strings.

--- javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Supplier.java
+++ javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Supplier.java
@@ -1,10 +1,13 @@
 package com.umer.javafunctional.functionalinterface;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class _Supplier {
 
-	private static final String DUMMY_DATABASE_CONNECTION_IRL = "jdbc://localhost:5432/users";
+	private static final String ANOTHER_DUMMY_URL = "Another dummy Url";
+
+	private static final String DUMMY_DATABASE_CONNECTION_URL = "jdbc://localhost:5432/users";
 
 	public static void main(String[] args) {
 		System.out.println("Connection string using imperative apporach: " 
@@ -12,16 +15,23 @@ public class _Supplier {
 
 		System.out.println("Connection string using declarative apporach: " 
 					+ getDBConnectionUrlSupplier.get());
+		
+		System.out.println("Connection string list using declarative apporach: " 
+				+ getDBConnectionUrlListSupplier.get());
 
 	}
 
 	// Imperative approach
 	static String getDBConnectionUrl() {
-		return DUMMY_DATABASE_CONNECTION_IRL;
+		return DUMMY_DATABASE_CONNECTION_URL;
 	}
 
 	// declarative approach
 	static Supplier<String> getDBConnectionUrlSupplier = () 
-			-> DUMMY_DATABASE_CONNECTION_IRL;
+			-> DUMMY_DATABASE_CONNECTION_URL;
 
+			static Supplier<List<String>> getDBConnectionUrlListSupplier = ()
+					->List.of(DUMMY_DATABASE_CONNECTION_URL, ANOTHER_DUMMY_URL);
+			
+			
 }
