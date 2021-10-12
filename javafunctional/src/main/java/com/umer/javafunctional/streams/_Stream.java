bad5b3f716ae3070d3a981617f2431d3f054f5c3
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-12 21:25:10 +0200

    Feature: Implemented base code which acts as scaffolding for the usage of streams.

--- javafunctional/src/main/java/com/umer/javafunctional/streams/_Stream.java
+++ javafunctional/src/main/java/com/umer/javafunctional/streams/_Stream.java
@@ -1,9 +1,41 @@
 package com.umer.javafunctional.streams;
 
+
+import java.util.List;
+
+
 public class _Stream {
 	
 	public static void main(String[] args) {
+		List<Person> people = List.of(
+				new Person("Umer", Gender.MALE), 
+				new Person("Maria", Gender.FEMALE),
+				new Person("Aisha", Gender.FEMALE), 
+				new Person("Alex", Gender.MALE), 
+				new Person("Alice", Gender.FEMALE)
+			);
 		
+	}
+	
+	static class Person {
+		private final String name;
+		private final Gender gender;
+
+		public Person(String name, Gender gender) {
+			this.name = name;
+			this.gender = gender;
+		}
+
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return "Person {" + "name='" + name + "\'" + ", gender=" + gender + "}";
+
+		}
+	}
+
+	enum Gender {
+		MALE, FEMALE;
 	}
 
 }
