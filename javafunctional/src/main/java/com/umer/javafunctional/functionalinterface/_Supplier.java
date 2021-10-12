f02fba6e2419a999a3ed758872a015f2e6b9e3a2
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-12 19:52:34 +0200

    Feature: Added declarative approach for getting dummy connection url.

--- javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Supplier.java
+++ javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Supplier.java
@@ -2,8 +2,17 @@ package com.umer.javafunctional.functionalinterface;
 
 public class _Supplier {
 	
+	private static final String DUMMY_DATABASE_CONNECTION_IRL = "jdbc://localhost:5432/users";
+
 	public static void main(String[] args) {
+		System.out.println(getDBConnectionUrl());
 		
+	}
+	
+	// declarative approach
+	static String getDBConnectionUrl() {
+		// A dummy database connection url
+		return DUMMY_DATABASE_CONNECTION_IRL;
 	}
 
 }
