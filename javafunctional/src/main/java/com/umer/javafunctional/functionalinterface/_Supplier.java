a0b1a0e80ee9f8995a153c9a2c8b68b25eac1368
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-12 19:59:00 +0200

    Feature: Using declarative approach of Supplier Object to get dummy connection String.

--- javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Supplier.java
+++ javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Supplier.java
@@ -1,18 +1,27 @@
 package com.umer.javafunctional.functionalinterface;
 
+import java.util.function.Supplier;
+
 public class _Supplier {
-	
+
 	private static final String DUMMY_DATABASE_CONNECTION_IRL = "jdbc://localhost:5432/users";
 
 	public static void main(String[] args) {
-		System.out.println(getDBConnectionUrl());
-		
+		System.out.println("Connection string using imperative apporach: " 
+					+ getDBConnectionUrl());
+
+		System.out.println("Connection string using declarative apporach: " 
+					+ getDBConnectionUrlSupplier.get());
+
 	}
-	
-	// declarative approach
+
+	// Imperative approach
 	static String getDBConnectionUrl() {
-		// A dummy database connection url
 		return DUMMY_DATABASE_CONNECTION_IRL;
 	}
+
+	// declarative approach
+	static Supplier<String> getDBConnectionUrlSupplier = () 
+			-> DUMMY_DATABASE_CONNECTION_IRL;
 
 }
