f1460f5012c37c853b01aa776a9df55191e6bd8e
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-17 19:02:10 +0200

    Feature: Coded use cases of optional.Empty

--- javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Optional.java
+++ javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Optional.java
@@ -1,9 +1,15 @@
 package com.umer.javafunctional.functionalinterface;
 
+import java.util.Optional;
+
 public class _Optional {
 
 	public static void main(String[] args) {
 		
+		final Optional<Object> empty = Optional.empty();
+		System.out.println("Current object is "+empty);
+		System.out.println(empty.isPresent());
+		System.out.println(empty.isEmpty());
 		
 
 	}
