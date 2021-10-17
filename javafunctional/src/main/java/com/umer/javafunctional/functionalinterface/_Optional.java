61da468813addef66fce7a53e946d070d256c58c
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-17 19:05:58 +0200

    Feature: Added use case of optional.of

--- javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Optional.java
+++ javafunctional/src/main/java/com/umer/javafunctional/functionalinterface/_Optional.java
@@ -6,11 +6,23 @@ public class _Optional {
 
 	public static void main(String[] args) {
 		
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("Use case of optional.empty");
+		System.out.println("---------------------------------------------------------------");
+		
 		final Optional<Object> empty = Optional.empty();
 		System.out.println("Current object is "+empty);
 		System.out.println(empty.isPresent());
 		System.out.println(empty.isEmpty());
 		
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("Use case of optional.of");
+		System.out.println("---------------------------------------------------------------");
+		
+		final Optional<String> hello = Optional.of("hello");
+		System.out.println(hello);
+		System.out.println(hello.isPresent());
+		System.out.println(hello.isEmpty());
 
 	}
 }
