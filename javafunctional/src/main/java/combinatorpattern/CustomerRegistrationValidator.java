f04af08dc77de042f882a3ecc367b648969ff604
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-19 16:57:48 +0200

    Feature: Implemented Combinator pattern for checking valid email.

--- javafunctional/src/main/java/combinatorpattern/CustomerRegistrationValidator.java
+++ javafunctional/src/main/java/combinatorpattern/CustomerRegistrationValidator.java
@@ -1,19 +1,26 @@
 package combinatorpattern;
 
 import java.util.function.Function;
-import static combinatorpattern.CustomerRegistrationValidator.ValidationResult; 
+import static combinatorpattern.CustomerRegistrationValidator.ValidationResult;
 
-public interface CustomerRegistrationValidator extends Function<Customer, ValidationResult>{
+
+public interface CustomerRegistrationValidator extends Function<Customer, ValidationResult> {
+
+	static CustomerRegistrationValidator isEmailValid() {
+		return customer -> customer.getEmail().contains("@") 
+				? ValidationResult.SUCCESS 
+				: ValidationResult.EMAIL_NOT_VALID;
+	}
 	
 	
 	
 	
 	
 	
-	enum ValidationResult{
-		SUCCESS,
-		PHONE_NUMBER_NOT_VALID,
-		EMAIL_NOT_VALID,
+	enum ValidationResult {
+		SUCCESS, 
+		PHONE_NUMBER_NOT_VALID, 
+		EMAIL_NOT_VALID, 
 		IS_NOT_AN_ADULT
 	}
 
