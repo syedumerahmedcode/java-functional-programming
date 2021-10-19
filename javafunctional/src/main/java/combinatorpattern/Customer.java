c97ff0f47fef43a445ba427a7668db7f920d08c1
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-19 12:30:29 +0200

    Feature: Created a basic customer class.

--- javafunctional/src/main/java/combinatorpattern/Customer.java
+++ javafunctional/src/main/java/combinatorpattern/Customer.java
@@ -1,5 +1,40 @@
 package combinatorpattern;
 
+import java.time.LocalDate;
+
 public class Customer {
+	
+	private final String name;
+	private final String email;
+	private final String phoneNumber;
+	private final LocalDate dateOfBirth;
+	
+	public Customer(String name, String email, String phoneNumber, LocalDate dateOfBirth) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return this.dateOfBirth;
+	}
+	
+	
+	
+	
 
 }
