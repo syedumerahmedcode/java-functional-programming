8f17c6c976f06c44625182db52e4373dce519cc9
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-20 20:29:46 +0200

    Feature: Added an interface which uses combinator pattern to list all the failure during the validation using EnumSet.

--- javafunctional/src/main/java/combinatorpattern/CustomerRegistrationValidatorSet.java
+++ javafunctional/src/main/java/combinatorpattern/CustomerRegistrationValidatorSet.java
@@ -1,15 +1,54 @@
 package combinatorpattern;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.EnumSet;
 import java.util.function.Function;
+import combinatorpattern.CustomerRegistrationValidatorSet.ValidationResultPossible;
 
-import combinatorpattern.CustomerRegistrationValidatorSet.ValidationResult;
+public interface CustomerRegistrationValidatorSet extends Function<Customer, EnumSet<ValidationResultPossible>>{
 
-public interface CustomerRegistrationValidatorSet extends Function<Customer, EnumSet<ValidationResult>>{
-
+	static final EnumSet<ValidationResultPossible> SUCCESS_ONLY= EnumSet.of(ValidationResultPossible.SUCCESS);
+	
+	static CustomerRegistrationValidatorSet isEmailValidSet() {
+		return customer -> customer.getEmail().contains("@") 
+				? SUCCESS_ONLY
+						: EnumSet.of(ValidationResultPossible.EMAIL_NOT_VALID);				
+	}
+	
+	static CustomerRegistrationValidatorSet isPhoneNumberValidSet() {
+		return customer -> customer.getPhoneNumber().startsWith("+0") 
+				? SUCCESS_ONLY
+						: EnumSet.of(ValidationResultPossible.PHONE_NUMBER_NOT_VALID);				
+	}
+	
+	static CustomerRegistrationValidatorSet isAnAdultSet() {
+        return customer -> Period.between(customer.getDateOfBirth(), LocalDate.now()).getYears() > 18 
+        		? SUCCESS_ONLY 
+        				: EnumSet.of(ValidationResultPossible.IS_NOT_AN_ADULT);
+    }
+	
+	default CustomerRegistrationValidatorSet and(CustomerRegistrationValidatorSet other) {
+		return customer ->{
+			EnumSet<ValidationResultPossible> thisResult=this.apply(customer);
+			EnumSet<ValidationResultPossible> otherResult= other.apply(customer);
+			
+			if(thisResult.equals(SUCCESS_ONLY))
+				return otherResult;
+			if(otherResult.equals(SUCCESS_ONLY))
+				return thisResult;
+			
+			EnumSet<ValidationResultPossible> combinedResult=EnumSet.copyOf(thisResult);
+			combinedResult.addAll(otherResult);
+			
+			return combinedResult;
+			
+			
+		};
+	}
 	
 	
-	enum ValidationResult {
+	enum ValidationResultPossible {
 		SUCCESS, 
 		PHONE_NUMBER_NOT_VALID, 
 		EMAIL_NOT_VALID, 
