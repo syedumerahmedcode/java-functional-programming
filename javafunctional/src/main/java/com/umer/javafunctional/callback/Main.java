1384ee887bb59ff120ef3189a33e414a42c98c7e
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-21 23:09:54 +0200

    Feature: Implemented Callback along with Callback with Runnable functionalities in the class.

--- javafunctional/src/main/java/com/umer/javafunctional/callback/Main.java
+++ javafunctional/src/main/java/com/umer/javafunctional/callback/Main.java
@@ -3,12 +3,36 @@ package com.umer.javafunctional.callback;
 import java.util.function.Consumer;
 
 public class Main {
+	private static final String EMPTY_STRING = "  ";
 	
 	public static void main(String[] args) {
-		/**
-		 * Last name is provided and Callback is null
-		 */
+		
+	
+		System.out.println(EMPTY_STRING);
+		System.out.println("------------------------------------------------------------");
+		System.out.println("Last name is provided and Callback is null.");
+		System.out.println("------------------------------------------------------------");
 		hello("John", "Montana", null);
+		System.out.println(EMPTY_STRING);
+		
+		System.out.println(EMPTY_STRING);
+		System.out.println("------------------------------------------------------------");
+		System.out.println("Last name is null and Callback is processing this condition.");
+		System.out.println("------------------------------------------------------------");
+		hello("John", null, value ->{
+			System.out.println("No last name provided for "+value);
+		});
+		System.out.println(EMPTY_STRING);
+		
+		System.out.println(EMPTY_STRING);
+		System.out.println("------------------------------------------------------------");
+		System.out.println("Last name is null and Callback With Runnable is processing this condition.");
+		System.out.println("------------------------------------------------------------");
+		helloWithRunnable("John", null, ()->{
+			System.out.println("No last name provided");
+		});
+		System.out.println(EMPTY_STRING);
+		
 	}
 	
 	
@@ -18,6 +42,15 @@ public class Main {
 			System.out.println(lastName);
 		}else {
 			callback.accept(firstName);
+		}
+	}
+	
+	static void helloWithRunnable(String firstName, String lastName, Runnable callback) {
+		System.out.println(firstName);
+		if(lastName!=null) {
+			System.out.println(lastName);
+		}else {
+			callback.run();
 		}
 	}
 	
