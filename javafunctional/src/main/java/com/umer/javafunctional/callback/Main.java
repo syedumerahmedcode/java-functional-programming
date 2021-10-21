c6b3f1ac01ff1ff83f4b42e4e2d4d21d9e6437f5
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-21 23:00:52 +0200

    Feature: Created a Java equivalent callback.

--- javafunctional/src/main/java/com/umer/javafunctional/callback/Main.java
+++ javafunctional/src/main/java/com/umer/javafunctional/callback/Main.java
@@ -1,10 +1,26 @@
 package com.umer.javafunctional.callback;
 
+import java.util.function.Consumer;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		
+		/**
+		 * Last name is provided and Callback is null
+		 */
+		hello("John", "Montana", null);
 	}
+	
+	
+	static void hello(String firstName, String lastName, Consumer<String> callback) {
+		System.out.println(firstName);
+		if(lastName!=null) {
+			System.out.println(lastName);
+		}else {
+			callback.accept(firstName);
+		}
+	}
+	
 	
 	/**
 	 * Sample java script code which does callback()
