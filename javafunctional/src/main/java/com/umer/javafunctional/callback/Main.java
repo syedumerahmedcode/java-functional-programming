c2f24c7033e6c98c6fcb195083029f1a9738de1d
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-21 22:54:46 +0200

    Feature: Added a sample javascript code which does call back. This function is commented out as it is only tehre as a reference.

--- javafunctional/src/main/java/com/umer/javafunctional/callback/Main.java
+++ javafunctional/src/main/java/com/umer/javafunctional/callback/Main.java
@@ -1,5 +1,21 @@
 package com.umer.javafunctional.callback;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * Sample java script code which does callback()
+	 */
+//	function hello(firstName, lastName, callback) {
+//		console.log(firstName);
+//		if(lastName) {
+//			console.log(lastName);
+//		}else {
+//			callback();
+//		}
+//	}
 
 }
