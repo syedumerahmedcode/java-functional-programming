397a965e503f68c2a56696dd86b634fa0473de7a
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-23 10:02:19 +0200

    Feature: Example of a lambda expression with one line.

--- javafunctional/src/main/java/com/umer/javafunctional/lambdas/Lambdas.java
+++ javafunctional/src/main/java/com/umer/javafunctional/lambdas/Lambdas.java
@@ -1,9 +1,17 @@
 package com.umer.javafunctional.lambdas;
 
+import java.util.function.Function;
+
 public class Lambdas {
 	
-	private static void main(String[] args) {
-		
+	private static final String EMPTY_STRING = "  ";
+	
+	public static void main(String[] args) {
+		Function<String, String> upperCaseName= name -> name.toUpperCase();
+		System.out.println("------------------------------------------------------------");
+		System.out.println("Labmda with a single line function");
+		System.out.println("------------------------------------------------------------");
+		System.out.println(upperCaseName.apply("Test String to uppercase"));
 
 	}
 
