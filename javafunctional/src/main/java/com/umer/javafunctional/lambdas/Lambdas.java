485e95d69fc0e707b17b97181c2d49c81c3ecae5
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-23 11:48:16 +0200

    Feature: Added an example of a biFunction that takes two inputs ---> String and Integer and returns a output ---> String.

--- javafunctional/src/main/java/com/umer/javafunctional/lambdas/Lambdas.java
+++ javafunctional/src/main/java/com/umer/javafunctional/lambdas/Lambdas.java
@@ -1,5 +1,6 @@
 package com.umer.javafunctional.lambdas;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Lambdas {
@@ -26,9 +27,28 @@ public class Lambdas {
 		System.out.println("------------------------------------------------------------");
 		System.out.println("Labmda with a multi line function");
 		System.out.println("------------------------------------------------------------");
-		System.out.println(upperCaseNameMultilineLogic.apply(""));
-		System.out.println(upperCaseNameMultilineLogic.apply("This line should not be converted to upper case"
-				+ " as the logic throws an Illegal argument exception"));
+		/**
+		 * This line should not be converted to upper case"
+				+ " as the logic throws an Illegal argument exception for balnk inputs
+		 */
+		//System.out.println(upperCaseNameMultilineLogic.apply(""));
+		System.out.println(upperCaseNameMultilineLogic.apply("Another test string to convert in upper case"));
+		System.out.println(EMPTY_STRING);
+		
+		
+		BiFunction<String,Integer, String> upperCaseNameAndDisplayAge= (name,age) -> {
+			// multi line logic
+			if(name.isBlank()) {
+				throw new IllegalArgumentException("name cannot be blank");
+			}
+			
+			return "The name of the person is "+name.toUpperCase()+" and the age is "+age;
+		};
+		System.out.println(EMPTY_STRING);
+		System.out.println("------------------------------------------------------------");
+		System.out.println("Labmda with a multi line Bifunction");
+		System.out.println("------------------------------------------------------------");
+		System.out.println(upperCaseNameAndDisplayAge.apply("Alice",99));		
 		System.out.println(EMPTY_STRING);
 		
 	}
