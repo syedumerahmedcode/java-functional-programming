33c7e9371d7dfca5f5607cb10a3b2c15ef11a738
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-23 11:40:32 +0200

    Feature: Added an example of a lambda expression that consists of multiple lines of code.

--- javafunctional/src/main/java/com/umer/javafunctional/lambdas/Lambdas.java
+++ javafunctional/src/main/java/com/umer/javafunctional/lambdas/Lambdas.java
@@ -12,7 +12,25 @@ public class Lambdas {
 		System.out.println("Labmda with a single line function");
 		System.out.println("------------------------------------------------------------");
 		System.out.println(upperCaseName.apply("Test String to uppercase"));
-
+		System.out.println(EMPTY_STRING);
+		
+		
+		Function<String, String> upperCaseNameMultilineLogic= name -> {
+			// multi line logic
+			if(name.isBlank()) {
+				throw new IllegalArgumentException("name cannot be blank");
+			}
+			return name.toUpperCase();
+		};
+		System.out.println(EMPTY_STRING);
+		System.out.println("------------------------------------------------------------");
+		System.out.println("Labmda with a multi line function");
+		System.out.println("------------------------------------------------------------");
+		System.out.println(upperCaseNameMultilineLogic.apply(""));
+		System.out.println(upperCaseNameMultilineLogic.apply("This line should not be converted to upper case"
+				+ " as the logic throws an Illegal argument exception"));
+		System.out.println(EMPTY_STRING);
+		
 	}
 
 }
