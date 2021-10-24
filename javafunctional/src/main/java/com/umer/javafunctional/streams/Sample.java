22863a1f99db59e91e5a03de045ac20dacbf8661
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-10-24 21:23:58 +0200

    Feature: Added an example of filetr, map and reduce in the project.

---
+++ javafunctional/src/main/java/com/umer/javafunctional/streams/Sample.java
@@ -0,0 +1,29 @@
+package com.umer.javafunctional.streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		// given the numbers, double the even numbers and total
+		
+		int result=0;
+		for(int e: numbers) {
+			if(e%2==0) {
+				result+=e*2;
+			}
+		}		
+		System.out.println("The end result with imperative approach is: "+result);
+		
+		System.out.println("The end result with declarative approach is: "+
+				numbers.stream()
+				.filter(e -> e%2==0)
+				.map(e -> e*2)
+				.reduce(0, Integer::sum));
+		
+		
+	}
+
+}
